4e171a92d9359e237b649f3e9fb6cbffa0f3c16c
Piotr Hałas <piotr.h@decodo.com>
2026-03-20 11:43:23 +0100

    chore: add declare(strict_types=1) to all PHP files

--- app/functions.php
+++ app/functions.php
@@ -1,7 +1,3 @@
 <?php
 
-/**
- * Here is your custom functions.
- */
-
 declare(strict_types=1);
--- healthcheck.php
+++ healthcheck.php
@@ -1,3 +1,6 @@
 <?php
+
+declare(strict_types=1);
+
 $response = @file_get_contents('http://localhost:8787/health');
 exit($response && str_contains($response, 'ok') ? 0 : 1);
